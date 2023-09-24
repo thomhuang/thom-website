57904f3aae398016fe2e6f54b34836345c637f20
= <thomashuang@Thomas-MacBook-Pro.local>
2023-09-23 18:02:44 -0700

    add light/dark mode and some organization and cleanup

--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,13 +1,10 @@
 import styles from './ErrorPage.module.css';
 
 export default function ErrorPage() {
-
   return (
-    <div>
-        <div className={styles.container}>
-            <h1>Oops!</h1>
-            <p>This page doesn't exist yet, or will never ... Sorry!</p>
-        </div>
+    <div className={styles.container}>
+        <h1>Oops!</h1>
+        <p>This page doesn't exist yet, or will never ... Sorry!</p>
     </div>
   );
 }
